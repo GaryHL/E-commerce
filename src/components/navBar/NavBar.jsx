8a5a97f6a24a7b247f62a102b1203fb572ed5161
Gary HL <103480668+GaryHL@users.noreply.github.com>
2022-09-27 20:59:58 +0200

    menu y cart base

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,17 +1,23 @@
 import React from "react";
+import { useState } from "react";
 import { RiShoppingBasket2Fill } from "react-icons/ri";
+import { AiOutlineSearch } from "react-icons/ai";
+import { RiMenuFill} from "react-icons/ri";
+
 
 import './navBar.scss'
 
 function NavBar() {
+  const [menuActive,setMenuActive]=useState(true);
+  const [cartActive,setCartActive]=useState(true);
   return (
     <>
       <nav className="container_nav">
-        <div className="icon_menu">Icon menu</div>
-        <div className="search_bar">Barra de busqueda</div>
-        <div className="icon_cart">Icon cart</div>
-        <div className="container_menu"> Contenedor de menu</div>
-        <div className="container_cart">Contenedor carrrito</div>
+        <div className="icon_menu" onClick={()=>setMenuActive(!menuActive) }><RiMenuFill/></div>
+        <input className="search_bar" type="search"></input>
+        <div className="icon_cart"><RiShoppingBasket2Fill/></div>
+        <div className={menuActive ? 'container_menu': 'container_menu active'}> <button onClick={()=> setMenuActive(!menuActive)}></button></div>
+        <div className={cartActive ? 'container_cart': 'container_cart active'}><button onClick={()=> setCartActive(!cartActive)}></button>Contenedor carrrito</div>
       </nav>
     </>
   );
