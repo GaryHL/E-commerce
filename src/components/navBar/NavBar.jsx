a71576e22300dbf99437de561231eff4ac38f00e
Gary HL <103480668+GaryHL@users.noreply.github.com>
2022-09-28 19:46:34 +0200

    Container_cart  Menu funcionales

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -10,14 +10,15 @@ import './navBar.scss'
 function NavBar() {
   const [menuActive,setMenuActive]=useState(true);
   const [cartActive,setCartActive]=useState(true);
+  
   return (
     <>
       <nav className="container_nav">
         <div className="icon_menu" onClick={()=>setMenuActive(!menuActive) }><RiMenuFill/></div>
         <input className="search_bar" type="search"></input>
-        <div className="icon_cart"><RiShoppingBasket2Fill/></div>
+        <div className="icon_cart" onClick={()=>setCartActive(!cartActive) }><RiShoppingBasket2Fill/></div>
         <div className={menuActive ? 'container_menu': 'container_menu active'}> <button onClick={()=> setMenuActive(!menuActive)}></button></div>
-        <div className={cartActive ? 'container_cart': 'container_cart active'}><button onClick={()=> setCartActive(!cartActive)}></button>Contenedor carrrito</div>
+        <div className={cartActive ? 'container_cart': 'container_cart active'}><button onClick={()=> setCartActive(!cartActive)}></button></div>
       </nav>
     </>
   );
